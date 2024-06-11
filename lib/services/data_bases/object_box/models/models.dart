274f420260a1efb03e14ff05920d3067305baf09
Vlad <info@dtlogickgroup.com.ua>
2024-06-11 11:36:15 +0300

    Add export file for object-box models

---
+++ lib/services/data_bases/object_box/models/models.dart
@@ -0,0 +1,9 @@
+export 'nom.dart';
+export 'price.dart';
+export 'storage.dart';
+export 'barcode.dart';
+export 'counterparty.dart';
+export 'order_nom.dart';
+export 'contract.dart';
+export 'discount.dart';
+export 'unit.dart';
